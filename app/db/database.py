a379b2edb921d819aa35e4efb45831ee01d3d23d
md <marceldiomare@gmail.com>
2024-11-15 00:10:24 +0300

    0003 added dependensies: dotenv, sqlalchemy, secret.py, database.py

---
+++ app/db/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
